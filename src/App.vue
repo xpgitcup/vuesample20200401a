4eca1f7bfc58b9d2548492df4b7ce391c4db6013
cupcup <xpli126@gmail.com>
2020-04-03 21:26:06 +0800

    调整了路由

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,44 @@
 <template>
     <div id="app">
-        <router-view></router-view>
+        <el-container>
+            <!-- 这里是页眉-->
+            <el-header>
+                <applicationTitle></applicationTitle>
+            </el-header>
+
+            <el-container>
+                <!-- 这里是菜单-->
+                <el-aside width="10%">
+                    <div v-if="$store.state.currentUser">
+                        <menus></menus>
+                    </div>
+                </el-aside>
+
+                <!--这里是主显示区-->
+                <el-main>
+                    <router-view></router-view>
+                </el-main>
+            </el-container>
+
+            <!-- 这里是页脚-->
+            <el-footer>
+                页脚
+            </el-footer>
+        </el-container>
     </div>
 </template>
 
 <script>
+    import applicationTitle from "./components/home/applicationTitle.vue";
+    import menus from './components/home/menus.vue'
+
+    export default {
+        name: "app",
+        components: {
+            applicationTitle,
+            menus
+        }
+    }
 </script>
 
 <style>
--- src/components/default.vue
+++ src/components/default.vue
@@ -1,40 +1,22 @@
 <template>
-    <el-container>
-        <!-- 这里是页眉-->
-        <el-header>
-            <applicationTitle></applicationTitle>
-        </el-header>
-
-        <el-container>
-            <!-- 这里是菜单-->
-            <el-aside width="10%">
-                <div v-if="$store.state.currentUser">
-                    <menus></menus>
-                </div>
-            </el-aside>
-
-            <!--这里是主显示区-->
-            <el-main>
-                <router-view></router-view>
-            </el-main>
-        </el-container>
-
-        <!-- 这里是页脚-->
-        <el-footer>
-            页脚
-        </el-footer>
-    </el-container>
+    <div v-if="$store.state.currentUser">
+        <status></status>
+    </div>
+    <div v-else>
+        <info></info>
+    </div>
 </template>
 
 <script>
-    import applicationTitle from "./home/applicationTitle.vue";
-    import menus from './home/menus.vue'
+
+    import Status from "./home/status";
+    import Info from "./home/info";
 
     export default {
         name: "default",
         components: {
-            applicationTitle,
-            menus
+            Info,
+            Status
         }
     }
 
